0567322ad52e3ee686fa5e4e7e3a9a55431af2d6
Davide Chiarelli <chiarellidavide99@gmail.com>
2025-04-28 19:54:32 +0200

    add auth

--- src/main/java/it/epicode/gestioneViaggi/controller/DipendenteController.java
+++ src/main/java/it/epicode/gestioneViaggi/controller/DipendenteController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +21,7 @@ public class DipendenteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("isAuthenticated()")
     public DipendenteResponseDTO save(@RequestBody @Valid NewDipendenteDTO dto){
         return dipendenteService.save(dto);
     }
@@ -43,6 +45,7 @@ public class DipendenteController {
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id){
         dipendenteService.delete(id);
